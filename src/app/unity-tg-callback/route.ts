c91f6b71d57f0e14a396b330b625012b96168aaf
Ian-potter <Ian@portkey.finance>
2024-01-26 19:00:57 +0800

    feat: 🎸 add tg

---
+++ src/app/unity-tg-callback/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import queryString from "query-string";
+
+export async function POST(request: Request) {
+  const formData = await request.formData();
+
+  const token = formData.get("token");
+
+  return NextResponse.redirect(
+    `http://localhost:53285?${queryString.stringify({
+      token,
+      provider: "Telegram",
+    })}`
+  );
+}
