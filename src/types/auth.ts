722d162cfa27bf72fe27381bcd279d3de7b31fb4
Ian-potter <Ian@portkey.finance>
2024-06-06 16:52:04 +0800

    feat: 🎸 show  approve detial

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,3 +1,5 @@
+import { IVerifyEntryParams } from 'src/utils/model';
+
 export interface GoogleAuthProps {
   scope?: string;
   prompt?: string;
@@ -12,7 +14,7 @@ export interface GoogleAuthProps {
   discoveryDocs?: string;
   children?: React.ReactNode;
   isOnlyGetToken?: boolean;
-  typeResponse?: "idToken" | "accessToken";
+  typeResponse?: 'idToken' | 'accessToken';
   fetchBasicProfile?: boolean;
 }
 
@@ -24,6 +26,7 @@ export interface BaseOpenLoginParam {
   network: string;
   publicKey: string;
   serviceURI: string;
+  approveDetail?: IVerifyEntryParams;
 }
 
 export type OpenLoginParamConfig = BaseOpenLoginParam & Record<string, string>;
